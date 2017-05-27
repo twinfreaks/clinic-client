566d7d3f95f9283f967bf20a177d4b0724cc102a
Vitalii Yaremchuk <vitalyar7@gmail.com>
2017-05-27 17:28:44 +0300

    gulp

--- client.js
+++ client.js
@@ -4,13 +4,21 @@ var express = require('express'),
     app = express(),
     path = require('path'),
     projectRoot = __dirname;
+
+app.set('port', (process.env.PORT || 3009));
+
 app.use(express.static(projectRoot));
 
-app.get('*', function (req, res) {
+// views is directory for all template files
+app.set('views', projectRoot + '/src');
+app.set('view engine', 'html');
+
+app.get('/*', function (req, res) {
   res.sendFile(path.join(projectRoot + '/index.html'));
 });
 
 var port = 3009;
 
-console.log('Server up and running on http://localhost:/' + port);
-app.listen(port);
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
